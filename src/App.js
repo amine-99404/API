fd2e5dee2372b41684dc986be2dd6b82e48f1be5
amine-99404 <saadaoui.amine95@gmail.com>
2021-02-25 09:31:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import UserList from './Component/UserList';
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <UserList/>
+  
+    </div>
+  );
+}
+
+export default App;
